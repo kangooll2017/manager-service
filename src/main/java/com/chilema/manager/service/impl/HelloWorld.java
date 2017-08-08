51259262611c711d0af0cb60832c36b12dfb5069
kangooll <328801750@qq.com>
2017-08-08 20:58:41 +0800

    框架搭设完成初次提交

---
+++ src/main/java/com/chilema/manager/service/impl/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.chilema.manager.service.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorld {
+
+	public void getAll(){
+		System.out.println("hello"); 
+	}
+}
